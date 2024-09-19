8af7c3fa1d643510986e34da7b3ba4510f15726e
migloww <miguelruizalzate04@gmail.com>
2024-09-19 07:05:26 -0500

    app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ values = 0.0
 act1="OFF"
 
 def on_publish(client,userdata,result):             #create function for callback
-    print("el dato ha sido publicado \n")
+    print("el dato ha sido publicado \n") 
     pass
 
 def on_message(client, userdata, message):
